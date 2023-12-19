dce2ac7d11439a9382e278d082d3bbe2a96b3c28
Dallin Robison <dallinrobison@gmail.com>
2023-12-18 22:35:49 -0700

    bugfix

--- dash.py
+++ dash.py
@@ -7,11 +7,6 @@ import seaborn as sns
 st.title('Deaths in the United States from Pneumonia and Influenza')
 deaths = pd.read_csv('./deaths.csv')
 
-
-st.markdown(
-    "Pneumonia and Influenza are deadly seasonal viruses. Using past data we hope to find information about trends in these viruses and their lethality to help prevent further harm and better care for the vulnerable around us."
-)
-
 decades_natl = deaths[(deaths['geo_level']  == 'National') & (deaths['age'] == 'All')].groupby('year').sum(numeric_only = True).reset_index()
 
 dcd_flu_plt = sns.barplot(decades_natl, x = 'year', y = 'flu_deaths', color = 'firebrick')
