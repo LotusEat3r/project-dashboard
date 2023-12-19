16d329e7abffcb06fd729946ed7b4f44b9f963d8
Dallin Robison <dallinrobison@gmail.com>
2023-12-18 22:45:59 -0700

    bugfix

--- dash.py
+++ dash.py
@@ -9,20 +9,19 @@ deaths = pd.read_csv('./deaths.csv')
 
 decades_natl = deaths[(deaths['geo_level']  == 'National') & (deaths['age'] == 'All')].groupby('year').sum(numeric_only = True).reset_index()
 
-dcd_flu_plt = sns.barplot(decades_natl, x = 'year', y = 'flu_deaths', color = 'firebrick')
-dcd_flu_plt.set(xlabel = "Year", ylabel = "Deaths From Influenza", title = "Deaths due to Influenza by Year")
-
-dcd_pneu_plt = sns.barplot(decades_natl, x = 'year', y = 'pneu_deaths', color = 'skyblue')
-dcd_pneu_plt.set(xlabel = "Year", ylabel = "Deaths From Pneumonia", title = "Deaths due to Pneumonia by Year")
-
 st.markdown("One of the first things we can see is the number of national deaths due to influenza and pneumonia for the United States between the years 2010 and 2018.")
 disease = st.selectbox("Select One:", ["Pneumonia", "Influenza"])
 
 if disease == "Pneumonia":
-    st.write("true")
+    sdcd_pneu_plt = sns.barplot(decades_natl, x = 'year', y = 'pneu_deaths', color = 'skyblue')
+    dcd_pneu_plt.set(xlabel = "Year", ylabel = "Deaths From Pneumonia", title = "Deaths due to Pneumonia by Year")
+    st.pyplot(dcd_pneu_plt.figure)
+if disease == "Influenza":
+    dcd_flu_plt = sns.barplot(decades_natl, x = 'year', y = 'flu_deaths', color = 'firebrick')
+    dcd_flu_plt.set(xlabel = "Year", ylabel = "Deaths From Influenza", title = "Deaths due to Influenza by Year")
+    st.pyplot(dcd_flu_plt.figure)
 
-st.pyplot(dcd_flu_plt.figure)
-st.pyplot(dcd_pneu_plt.figure)
+
 
 #species = st.selectbox("Select a Species", penguins['species'].unique())
 #island = st.selectbox("Select an Island", penguins[penguins['species'] == species]['island'].unique())
